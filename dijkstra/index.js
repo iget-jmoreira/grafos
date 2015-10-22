476da96763573e169aecc07b538fc4e05066c823
jmoreira <jmoreira@iget.com.br>
2015-10-22 16:24:52 -0300

    busca dijkstra

--- dijkstra/index.js
+++ dijkstra/index.js
@@ -35,23 +35,23 @@ nVERTICE.fillText("6", x+e+e-3, y+e+4);
 // desenha o peso de cada aresta
 pARESTA = canvas.getContext('2d');
 nVERTICE.font = "bold 11px sans-serif";
-nVERTICE.fillText("1", x+(e/2)-12, y+(e/2)+10);
+nVERTICE.fillText("7", x+(e/2)-12, y+(e/2));
 nVERTICE.font = "bold 11px sans-serif";
-nVERTICE.fillText("3", x+(e/2), y-5);
+nVERTICE.fillText("9", x+e+3, y+(e/2)+5);
 nVERTICE.font = "bold 11px sans-serif";
-nVERTICE.fillText("1", x+e-10, y+(e/2)+4);
+nVERTICE.fillText("14", x+e+(e/2)+3, y+(e/2));
 nVERTICE.font = "bold 11px sans-serif";
-nVERTICE.fillText("2", x+e+(e/2), y+e+13);
+nVERTICE.fillText("10", x+(e/2), y+e-3);
 nVERTICE.font = "bold 11px sans-serif";
-nVERTICE.fillText("3", x+e+(e/2)-12, y+(e/2));
+nVERTICE.fillText("15", x+(e/2)-15, y+e+(e/2)+10);
 nVERTICE.font = "bold 11px sans-serif";
-nVERTICE.fillText("2", x+e+(e/2), y-5);
+nVERTICE.fillText("11", x+e-15, y+e+(e/2));
 nVERTICE.font = "bold 11px sans-serif";
-nVERTICE.fillText("-3", x+e+e-12, y+(e/2));
+nVERTICE.fillText("2", x+e+(e/2)-3, y+e+13);
 nVERTICE.font = "bold 11px sans-serif";
-nVERTICE.fillText("3", x+e+e+(e/2)+4, y+(e/2)+10);
+nVERTICE.fillText("6", x+e+(e/2)-3, y+e+e-3);
 nVERTICE.font = "bold 11px sans-serif";
-nVERTICE.fillText("2", x+e+e+(e/2), y-5);
+nVERTICE.fillText("2", x+e+e+3, y+e+(e/2)+3);
 
 // desenha os vertices
 VERTICES = canvas.getContext('2d');
@@ -116,92 +116,96 @@ ARESTAS.lineTo(x+e-rr, y+e);
 ARESTAS.moveTo(x+rd,y+e+rd);
 ARESTAS.lineTo(x+e-rd, y+e+e-rd);
 // 3-4
-ARESTAS.moveTo(x+e,y+e);
-ARESTAS.lineTo(x+e, y+e+e);
+ARESTAS.moveTo(x+e,y+e+rr);
+ARESTAS.lineTo(x+e, y+e+e-rr);
 // 3-6
-ARESTAS.moveTo(x+e,y+e);
-ARESTAS.lineTo(x+e+e, y+e);
+ARESTAS.moveTo(x+e+rr,y+e);
+ARESTAS.lineTo(x+e+e-rr, y+e);
 // 4-5
-ARESTAS.moveTo(x+e,y+e+e);
-ARESTAS.lineTo(x+e+e, y+e+e);
+ARESTAS.moveTo(x+e+rr,y+e+e);
+ARESTAS.lineTo(x+e+e-rr, y+e+e);
 // 5-6
-ARESTAS.moveTo(x+e+e,y+e+e);
-ARESTAS.lineTo(x+e+e, y+e);
+ARESTAS.moveTo(x+e+e,y+e+e-rr);
+ARESTAS.lineTo(x+e+e, y+e+rr);
 
 ARESTAS.stroke();
 
 
 
-var graph_array = [
-	['a', 'b', 7],
-	['a', 'c', 9],
-	['a', 'f', 14],
-	['b', 'c', 10],
-	['b', 'd', 15],
-	['c', 'd', 11],
-	['c', 'f', 2],
-	['d', 'e', 6],
-	['e', 'f', 9],
-];
+// var graph_array = [
+// 	['a', 'b', 7],
+// 	['a', 'c', 9],
+// 	['a', 'f', 14],
+// 	['b', 'c', 10],
+// 	['b', 'd', 15],
+// 	['c', 'd', 11],
+// 	['c', 'f', 2],
+// 	['d', 'e', 6],
+// 	['e', 'f', 9],
+// ];
 
 
-var source = "a";
-var target = "e";
+// var source = "a";
+// var target = "e";
 
-for(i = 0; i < vertices.length; i++){
-	for (l = 0; l < vertices.length; l++) {
-		if((i != l) && (vertices[i] == vertices[l])){
-			vertices.splice(i,1);
-			i--;
-		}
-	};
-}
-
-var dist = [];
-var previous = [];
-vertices.forEach(function(vertex){
-	dist[vertex] = 0;
-	previous[vertex] = null;
-});
-
- 
-dist[source] = 0;
-Q = vertices;
-while (Q.length > 0) {
- 
-//         // TODO - Find faster way to get minimum
-	$min = INF;
-	foreach ($Q as $vertex){
-	    if ($dist[$vertex] < $min) {
-	        $min = $dist[$vertex];
-	        $u = $vertex;
-	    }
-	}
- 
-//         $Q = array_diff($Q, array($u));
-//         if ($dist[$u] == INF or $u == $target) {
-//             break;
-//         }
- 
-//         if (isset($neighbours[$u])) {
-//             foreach ($neighbours[$u] as $arr) {
-//                 $alt = $dist[$u] + $arr["cost"];
-//                 if ($alt < $dist[$arr["end"]]) {
-//                     $dist[$arr["end"]] = $alt;
-//                     $previous[$arr["end"]] = $u;
-//                 }
-//             }
-//         }
-//     }
-//     $path = array();
-//     $u = $target;
-//     while (isset($previous[$u])) {
-//         array_unshift($path, $u);
-//         $u = $previous[$u];
-//     }
-//     array_unshift($path, $u);
-//     return $path;
+// for(i = 0; i < vertices.length; i++){
+// 	for (l = 0; l < vertices.length; l++) {
+// 		if((i != l) && (vertices[i] == vertices[l])){
+// 			vertices.splice(i,1);
+// 			i--;
+// 		}
+// 	};
 // }
+
+// var dist = [];
+// var previous = [];
+// vertices.forEach(function(vertex){
+// 	dist[vertex] = 0;
+// 	previous[vertex] = null;
+// });
+
+ 
+// dist[source] = "inf";
+// Q = vertices;
+// while (Q.length > 0) {
+ 
+// //         // TODO - Find faster way to get minimum
+// 	var min = "inf";
+// 	var u;
+// 	Q.forEach(function(vertex){
+// 		if(dist[vertex] != min){
+// 			min = dist[vertex];
+// 			u = vertex;
+// 		}
+// 	});
+
+// 	Array.prototype.diff = function(a) {
+//     	return this.filter(function(i) {return a.indexOf(i) < 0;});
+// 	}; 
+//  	Q = Q.diff(u);
+
+//     if ((dist[u] == "inf") || (u == target)) {
+//         break;
+//     }
+
+//     if(typeof neighbours[u] != 'undefined'){
+//     	neighbours[u].forEach(function(arr){
+//     		alt = dist[u] + arr["cost"];
+//     		if(alt < dist[arr["end"]]){
+//     			previous[arr["end"]] = u;
+//     		}
+//     	});
+//     }
+// }
+
+// path = [];
+// u = target;
+// while (typeof previous[u] != 'undefined') {
+// 	path.splice(0,0,u);
+//     u = previous[u];
+// }
+// path.splice(0,0,u);
+// console.log(path);
  
 // $path = dijkstra($graph_array, "a", "e");
  
